c86f114fed78039502f15e7e8a54e430c47ba774
thibautlecee <leceethibaut@gmail.com>
2023-12-21 20:57:55 +0100

    deploy with folders needed

--- src/app.ts
+++ src/app.ts
@@ -27,6 +27,7 @@ app.use(cors(corsOptions));
 app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+
 app.use(express.urlencoded({extended: true}));
 app.get('/', (req, res) => {
     res.send({title: 'API is running'});
